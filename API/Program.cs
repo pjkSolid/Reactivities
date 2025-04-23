9dbbea83d3ae933031b59cfbdd1680d343e506c0
paul-kane-kainos <paul.kane@kainos.com>
2025-04-23 15:08:01 +0100

    EndOfSection4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -12,7 +14,10 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 }); 
 
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => 
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
 
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
